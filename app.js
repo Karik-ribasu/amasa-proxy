bf374952c593dc5115b89490d827902b7542c2c0
karik-ribasu <mechamocaique@gmail.com>
2024-10-04 02:13:54 -0300

    add queue, change request params and processing

--- app.js
+++ app.js
@@ -1,54 +1,54 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
+const queue = require('express-queue');
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+app.use(queue({ activeLimit: 1, queuedLimit: -1 }));
 
-async function getSwapData({
-  srcToken,
-  dstToken,
-  amount,
-  from,
-  slippage = 1,
-}) {
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function getSwapData(requests) {
   const token = process.env.ONE_INCH_TOKEN;
-  const query = `https://api.1inch.dev/swap/v6.0/42161/swap?src=${srcToken}&dst=${dstToken}&amount=${amount.toString()}&from=${from}&slippage=${slippage}&receiver=${from}&disableEstimate=true&compatibility=true`;
+  const result = [];
   try {
-    const response = await fetch(query, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        accept: "application/json"
-      },
-    });
-    const data = await response.json()
-    console.log(data)
-    return data;
+    for (let request of requests) {
+      const { srcToken, dstToken, amount, from } = request;
+      const query = `https://api.1inch.dev/swap/v6.0/42161/swap?src=${srcToken}&dst=${dstToken}&amount=${amount.toString()}&from=${from}&slippage=${2}&receiver=${from}&disableEstimate=true&compatibility=true`;
+      const response = await fetch(query, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          accept: "application/json",
+        },
+      });
+      await delay(1000);
+      const data = await response.json();
+      result.push(data);
+    }
+    console.log(result);
+    return result;
   } catch (error) {
-    console.log(error)
+    console.log(error);
     throw error;
   }
 }
 
-app.post("/1inch/swap-data", async (req, res) => {
-  const { srcToken, dstToken, amount, from } = req.body;
-  console.log({ srcToken, dstToken, amount, from })
+app.post("/1inch/swap-data", (req, res) => {
+  const { args } = req.body;
   try {
-    const data = await getSwapData({ srcToken, dstToken, amount, from });
-    res.json({ data });
+    getSwapData(args).then((result) => res.json({ data: result }));
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(500).json({ error: error });
   }
 });
 
-const server = app.listen(port, () =>
-  console.log(`listening on port ${port}!`)
-);
+const server = app.listen(port, () => console.log(`listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
